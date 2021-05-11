d13f5a2a21e59384f3753cd28f5a8f86550dfb8a
Carlos Rodrigues <crodrigsantos@gmail.com>
2021-05-11 18:43:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import { Header } from './components/Header';
+
+import './styles/global.scss';
+
+
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const state = {
+    selectedGenreId, setSelectedGenreId
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      
+      <SideBar state={state} />
+     
+      <Content state={state} />
+      
+    </div>
+  )
+}
