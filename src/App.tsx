defa7d6258f53bece6e4df8a74fb8b724c965663
Carlos Rodrigues <crodrigsantos@gmail.com>
2021-05-11 19:35:03 -0300

    Desafio finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import { useState } from 'react';
 
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
-import { Header } from './components/Header';
 
 import './styles/global.scss';
-
-
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
